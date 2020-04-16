16c39644e414961388c06cd0c773fb88ff5f713c
John Carter <johncarter352@gmail.com>
2020-04-16 14:46:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,6 +9,16 @@ class TicTacToe
   end
   
   def display_board
-    
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def input_to_index(input)
+  input.to_index - 1 
+end
+  
   
 end
